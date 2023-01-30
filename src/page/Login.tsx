31a340979dc80d9503400938f19f299ef7dd983c
danpoj <jws970306@khu.ac.kr>
2023-01-30 16:28:38 +0900

    chore: eslint & prettier

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -2,9 +2,9 @@ import Button from '@component/Button'
 
 const Login = () => {
   return (
-    <div className='w-full h-full flex flex-col gap-10 items-center justify-center'>
-      <h1 className='font-black text-stone-600 text-4xl'>Admin 🐾</h1>
-      <Button intent='secondary' size='medium'>
+    <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
+      <h1 className="font-black text-stone-600 text-4xl">Admin 🐾</h1>
+      <Button intent="secondary" size="medium">
         Login 👋
       </Button>
     </div>
